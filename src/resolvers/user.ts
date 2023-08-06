3ee660ab59ba7268432957994a7b665fd13c887c
Triada <adiskadj@gmail.com>
2023-08-06 16:06:03 +0600

    added login to userResolver, gitignored dist folder for now

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -10,8 +10,10 @@ import {
   Arg,
   InputType,
   Field,
+  ObjectType,
 } from "type-graphql";
 import argon2 from "argon2";
+import { log } from "console";
 
 @InputType()
 class UsernamePasswordInput {
@@ -19,6 +21,23 @@ class UsernamePasswordInput {
   username: string;
   @Field()
   password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(()=>[FieldError], {nullable:true})
+  errors?: FieldError[]
+
+  @Field(()=>User, {nullable:true})
+  user?: User
 }
 
 @Resolver(User)
@@ -35,6 +54,33 @@ export class UserResolver {
     });
     await em.persistAndFlush(user);
     return user;
+  }
+
+  @Mutation(()=>UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ) : Promise<UserResponse> {
+    const user = await em.findOne(User, {username:options.username})    
+    if(!user){      
+      return{
+        errors:[{
+          field: 'username',
+          message: 'that username doesnt exist'
+        }]
+      }
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+    if(!valid){
+      return{
+        errors:[{
+          field: 'password',
+          message: 'incorrect password'
+        }]
+      }
+    }
+    return {user,};
   }
 
   @FieldResolver(() => String)
